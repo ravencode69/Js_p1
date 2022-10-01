3171197ee130edd495821bda72f7604bb7152472
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2022-10-01 17:40:51 +0530

    functions

--- JS_fundamentals_part2/script.js
+++ JS_fundamentals_part2/script.js
@@ -1,12 +1,18 @@
 'use strict';
-let hasDriversLicense = false;
-let passTest = true;
+// let hasDriversLicense = false;
+// let passTest = true;
 
-//1--> shows error messages
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("drive");
+// //1--> shows error messages
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("drive");
 
-//2-->reserved keywords
-const private = 'jill';
+// //2-->reserved keywords
+// const private = 'jill';
 
 //function
+function logger() {
+    console.log('my name is raven ');
+}
+
+//calling /running / invoking the function 
+logger();
