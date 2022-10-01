402c468b7d7a42a122b5ee4555ca7e1cd84e7e64
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2022-10-01 17:38:34 +0530

    Structural changes

---
+++ JS_fundamentals_part2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+let hasDriversLicense = false;
+let passTest = true;
+
+//1--> shows error messages
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("drive");
+
+//2-->reserved keywords
+const private = 'jill';
+
+//function
