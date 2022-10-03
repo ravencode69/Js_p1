4f92bbf841008bb78e8d391c0a52f6e249183659
ravencode69 <1906569@kiit.ac.in>
2022-10-03 14:27:05 +0530

    JS files

--- JS_fundamentals_part2/script.js
+++ JS_fundamentals_part2/script.js
@@ -10,9 +10,19 @@
 // const private = 'jill';
 
 //function
-function logger() {
-    console.log('my name is raven ');
-}
+// function logger() {
+//     console.log('my name is raven ');
+// }
 
 //calling /running / invoking the function 
-logger();
+// logger();
+// logger();
+
+function foodProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const res = `We have ${apples} apples and ${oranges} oranges`;
+    return res;
+}
+
+const amt = foodProcessor(3, 4);
+console.log(amt);
